def7dbeb37237109e5c5c8f101cf42585bcefb8a
Aditya Kumar <adityakumar@Adityas-MacBook-Pro-5.local>
2021-11-26 17:50:44 +0530

    adding

---
+++ if-else.c
@@ -0,0 +1,44 @@
+                                    //IF ELSE 
+// SYNTAX:
+// if(test_expression)
+// {
+//     statement 1;
+//     statement 2;
+//     ...
+// }
+// else {
+// statement 1;
+// statement 2;
+// }
+//Why do we use if else 
+//if else is used where ever we need to impliment condition
+//Example to find whether a number is even or not 
+// #include <stdio.h>
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     if(n%2==0){
+//         printf("Even");
+
+//     }
+//     else{
+//         printf("Odd");
+//     }
+// }
+// ELSE IF 
+// program to  
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    if(n==2){
+        printf("2\n");
+
+    }
+    else if (n==3){
+        printf("3\n");
+    }
+    else{
+        printf("neither 3 not 4\n");
+    }
+}
