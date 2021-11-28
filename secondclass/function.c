083ee094f2505cacce6eceff3e9299cb220c02a2
Aditya Kumar <adityakumar@Adityas-MacBook-Pro-5.local>
2021-11-28 16:56:15 +0530

    adding

---
+++ secondclass/function.c
@@ -0,0 +1,8 @@
+//                         FUNCTIONS IN C
+// here are three aspects of a C function.
+
+// Function declaration A function must be declared globally in a c program to tell the compiler about the function name, function parameters, and return type.
+
+// Function call Function can be called from anywhere in the program. The parameter list must not differ in function calling and function declaration. We must pass the same number of functions as it is declared in the function declaration.
+
+// Function definition It contains the actual statements which are to be executed. It is the most important aspect to which the control comes when the function is called. Here, we must notice that only one value can be returned from the function.
---
+++ secondclass/union.c
@@ -0,0 +1,21 @@
+                                    //UNIONS 
+
+// #include <stdio.h>
+// #include<string.h>
+// union abc  // defining a union 
+// {  
+//    int a;  // defining datatypes in it 
+//    char b[10];   
+// }var;  // making objects 
+// int main()  
+// {  
+//   var.a = 66; 
+   
+//   printf("%d\n", var.a);
+//   strcpy(var.b,"adityakumar");  
+//   printf("%s",var.b);
+// }   
+// WHY UNION IF WE HAVE STRUCTURE :-
+// 1. Union used shared memory like the variable with highest storage shares it's storage 
+// THEN WHY STRUCTURE :-
+// BECAUSE IN STRUCTURE WE CAN USE ALL ELEMENTS SIMULTANEOUSLY THAT DOES'NT HAPPEN IN UNIONS 
