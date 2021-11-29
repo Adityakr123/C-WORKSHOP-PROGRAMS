7d3b136fe8a696a18fac6c18220876d694f53ddd
Aditya Kumar <adityakumar@Adityas-MacBook-Pro-5.local>
2021-11-29 15:45:58 +0530

    adding

--- secondclass/function.c
+++ secondclass/function.c
@@ -1,8 +1,27 @@
 //                         FUNCTIONS IN C
-// here are three aspects of a C function.
+// three aspects of a C function.
 
-// Function declaration A function must be declared globally in a c program to tell the compiler about the function name, function parameters, and return type.
+// Function declaration A 
 
-// Function call Function can be called from anywhere in the program. The parameter list must not differ in function calling and function declaration. We must pass the same number of functions as it is declared in the function declaration.
+// Function call Function 
 
-// Function definition It contains the actual statements which are to be executed. It is the most important aspect to which the control comes when the function is called. Here, we must notice that only one value can be returned from the function.
+// Function definition 
+
+// Syntax 
+// return_type function_name(data_type parameter...){  
+// //code to be executed  
+// }  
+
+#include<stdio.h>  
+void printName();  //function declaration //function prototyping means telling the compiler that this is a function . here void means return type of the function and this :- () means the input type  
+
+int main ()  
+{  
+    printf("Hello\n");  
+    printName();  //function calling 
+    return 0;
+}  
+void printName()  // function definition 
+{  
+    printf("Aditya Kumar");  
+}  
---
+++ secondclass/function1.c
@@ -0,0 +1,9 @@
+// function with return type and input type
+// write a program to add two number using functions
+#include<stdio.h>
+float add (float a,float b);// float before add means what would be return type of this function 
+//and float a and float b means type of input what would they be 
+int main(){
+float a,b;
+
+}
