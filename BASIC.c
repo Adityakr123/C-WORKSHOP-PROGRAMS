b95502e8695bd52c16414187da2c129bc8badb4a
Aditya Kumar <adityakr365@gmail.com>
2022-04-08 13:23:46 +0530

    adding

---
+++ BASIC.c
@@ -0,0 +1,4 @@
+#include<stdio.h>//standard input output header file header file is the adress of functions
+int main(){// this is the main function inside which  main code come's this function is initiated automatically 
+    printf("MTC WORKSHOP");
+}
